a18724d34b07c283d9e9cd055603759c19f4f1d4
Rcnald <ronaldomjunior05@gmail.com>
2023-03-06 21:21:23 -0300

    main process

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         'color-background': 'var(--background-color)',
         'color-text': 'var(--text-color)',
         'color-text-constrast':'var(--text-color-constrast)',
+        'color-notification':'var(--notification-color)',
+        'color-notification-dark':'var(--notification-color-dark)',
+        'color-line':'var(--line-color)',
+        'color-card':'var(--card-color)',
       }
     },
   },
