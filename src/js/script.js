79b00dd77d817e4718d80443776e8809676fe952
Rcnald <ronaldomjunior05@gmail.com>
2023-03-08 20:03:35 -0300

    API incomplete

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,21 @@
+const cardValue = document.getElementsByClassName('js-card-value')
+
 fetch("https://cors-everywhere.onrender.com/https://api.hgbrasil.com/finance")
     .then(response => response.json())
     .then(data =>{
         const results = data.results;
-        console.log(results)
+        const stocks = results.stocks;
+
+        let i = 0
+
+        for(let stock in stocks){
+            if(i <= 1){
+                cardValue[i].textContent = `${stocks[stock].variation}%`;
+
+                if(stocks[stock].variation < 0){
+                    cardValue[i].classList.add('u-negative')
+                }
+            }
+            i++
+        }
     })
